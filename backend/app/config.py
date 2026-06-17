3d6da1ae18ea221cc85ad8cd64250e1be8cce2c3
jay rathod <rathodjay4397@gmail.com>
2026-06-18 00:46:15 +0530

    feat: add Pydantic-based configuration management for environment variables and CORS origins

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,10 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     groq_api_key: str
-<<<<<<< HEAD
-=======
     cors_origins: str = "http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000,http://localhost:5174,http://127.0.0.1:5174,http://172.23.144.180:5174,http://172.23.144.180:5173"
->>>>>>> release1.0
 
     class Config:
         env_file = ".env"
